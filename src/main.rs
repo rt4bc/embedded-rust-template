a6b9e73f8bda4dc4c273d50e9b68dc76b80fd9a1
bochao <bochao.me@gmail.com>
2024-07-21 08:32:27 +0800

    update env configurations

--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,28 @@
-#![no_std]
 #![no_main]
+#![no_std]
 
 use cortex_m_rt::entry;
-use embedded_hal::digital::v2::OutputPin;
-use stm32f4xx_hal::{pac, prelude::*};
+use panic_halt as _;
 
-#[allow(unused_imports)]
-use panic_halt; // When a panic occurs, stop the microcontroller
+use stm32f4xx_hal::{pac, prelude::*};
 
 #[entry]
 fn main() -> ! {
-    
+    // 获取对外设的访问权限
+    //将 PAC（Peripheral Access Crate）的变量命名为 dp 是一种常见的惯例
+    //dp 代表 "Device Peripherals"（设备外设）。
     let dp = pac::Peripherals::take().unwrap();
 
-
+    // Cortex-M 处理器中，通常还有 cp（Core Peripherals），用于访问核心特定的外设。使用 dp 可以清楚地与 cp 区分开。
+    // 配置系统时钟
     let rcc = dp.RCC.constrain();
-    // - Configure system clocks
-    // 8 MHz must be used for the Nucleo-F401RE board according to manual
-    let clocks = rcc.cfgr.use_hse(8.MHz()).freeze();
+    let clocks = rcc.cfgr.use_hse(8.MHz()).sysclk(180.MHz()).freeze();
 
+    // 配置 GPIO
+    let gpiok = dp.GPIOK.split();
+    let mut green_led = gpiok.pk5.into_push_pull_output();
 
     loop {
- 
+        green_led.toggle();
     }
 }
