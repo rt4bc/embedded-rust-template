f027369daf8c42dd7cf3db19660f5219599f80ce
BoChao Liu <bochao@BoChaos-Mini.lan>
2024-08-18 17:26:38 +0800

    update

--- src/main.rs
+++ src/main.rs
@@ -1,37 +1,53 @@
 #![no_main]
 #![no_std]
 
-mod test;
+mod scratch;
 
+use cortex_m::iprintln;
 use cortex_m_rt::entry;
+
 use panic_halt as _;
 
 use stm32f4xx_hal::{pac, prelude::*};
 
 #[entry]
 fn main() -> ! {
+    {
+        scratch::asm_test();
+    }
+
     // 获取对外设的访问权限
     //将 PAC（Peripheral Access Crate）的变量命名为 dp 是一种常见的惯例
     //dp 代表 "Device Peripherals"（设备外设）。
-    let dp = pac::Peripherals::take().unwrap();
-
     // Cortex-M 处理器中，通常还有 cp（Core Peripherals），用于访问核心特定的外设。使用 dp 可以清楚地与 cp 区分开。
     // 配置系统时钟
+    let dp = pac::Peripherals::take().unwrap();
     let rcc = dp.RCC.constrain();
-    let clocks = rcc.cfgr.use_hse(8.MHz()).sysclk(180.MHz()).freeze();
+    let _clocks = rcc.cfgr.use_hse(8.MHz()).sysclk(180.MHz()).freeze();
+
+    // cp 是cortex_m peripherals
+    // 在launch里配置了
+    // "swoConfig": {
+    //     "enabled": true,
+    //     "cpuFrequency": 180000000,
+    //     "swoFrequency": 2000000,
+    //     "source": "probe",
+    //     "decoders": [
+    //       {
+    //         "port": 0,
+    //         "type": "console",
+    //         "label": "ITM"
+    //       }
+    //     ]
+    //   }
+    let mut cp = cortex_m::Peripherals::take().unwrap();
+    let stim0 = &mut cp.ITM.stim[0];
+    iprintln!(stim0, "Hello, ITM!");
 
     // 配置 GPIO
     let gpioa = dp.GPIOA.split();
     let mut green_led = gpioa.pa5.into_push_pull_output();
     let mut _i = 0;
-
-    {
-        if test::asm_test::asm_test_1() == 0 {
-            loop {}
-        }
-
-        test::itm_test();
-    }
 
     loop {
         green_led.toggle();
---
+++ src/scratch.rs
@@ -0,0 +1,41 @@
+use core::arch::asm;
+
+pub fn asm_test() {
+    unsafe {
+        asm!("nop");
+    }
+
+    unsafe {
+        let a:u32;
+        asm!(
+            "mov {}, 1",
+            out(reg) a
+        );
+        assert_eq!(a, 1);
+    }
+
+    unsafe {
+        let i:u32 = 3;
+        let o:u32;
+        asm!(
+            "mov {0}, {1}",
+            "add {0}, 5",
+            out(reg) o,
+            in(reg) i,
+        );
+        assert_eq!(o, 8);
+    }
+
+    unsafe {
+        let i:u32 = 3;
+        let o:u32;
+        asm!(
+            "mov {0}, {tmp}",
+            "add {0}, {1}",
+            out(reg) o,
+            in(reg) i,
+            tmp = in(reg) 10,
+        );
+        assert_eq!(o, 13);
+    }
+}
