7e4ae14aa25b83b0252e95b26c997685b5a8effc
BoChao Liu <bochao@BoChaos-Mini.lan>
2024-09-15 10:31:14 +0800

    updates

--- src/main.rs
+++ src/main.rs
@@ -2,10 +2,17 @@
 #![no_std]
 
 
+use cortex_m;
 use cortex_m_rt::entry;
+use stm32f1xx_hal::{pac, prelude};
 use panic_halt as _;
+
 
 #[entry]
 fn main() -> ! {
-  
+    // Get access to the core peripherals from the cortex-m crate
+    let cp = cortex_m::Peripherals::take().unwrap();
+    // Get access to the device specific peripherals from the peripheral access crate
+    let dp = stm32f1xx_hal::pac::Peripherals::take().unwrap();
+    loop {}
 }
